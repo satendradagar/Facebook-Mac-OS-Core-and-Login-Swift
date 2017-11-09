355a7b4528d130725c4bc4bdf613922a42608fcc
Satendra Singh <satendra.techmac@gmail.com>
2017-11-09 20:44:27 +0530

    Added Facebook Corekit and Login base code for Swift

---
+++ Facebook Mac Core/Facebook Mac Core/Facebook Mac Core/Facebook_Mac_Core.h
@@ -0,0 +1,19 @@
+//
+//  Facebook_Mac_Core.h
+//  Facebook Mac Core
+//
+//  Created by Satendra Singh on 25/10/17.
+//  Copyright © 2017 CoreBits. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Facebook_Mac_Core.
+FOUNDATION_EXPORT double Facebook_Mac_CoreVersionNumber;
+
+//! Project version string for Facebook_Mac_Core.
+FOUNDATION_EXPORT const unsigned char Facebook_Mac_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Facebook_Mac_Core/PublicHeader.h>
+
+
---
+++ Facebook Mac Login/Facebook Mac Login/Facebook_Mac_Login.h
@@ -0,0 +1,19 @@
+//
+//  Facebook_Mac_Login.h
+//  Facebook Mac Login
+//
+//  Created by Satendra Singh on 24/10/17.
+//  Copyright © 2017 CoreBits. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Facebook_Mac_Login.
+FOUNDATION_EXPORT double Facebook_Mac_LoginVersionNumber;
+
+//! Project version string for Facebook_Mac_Login.
+FOUNDATION_EXPORT const unsigned char Facebook_Mac_LoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Facebook_Mac_Login/PublicHeader.h>
+
+
