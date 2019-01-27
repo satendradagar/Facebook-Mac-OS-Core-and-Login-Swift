7c73c890588b68898c3bee3deebb7181197589ff
Satendra Touch <satendradagar@gmail.com>
2019-01-27 15:38:49 +0530

    added event kit extension as separate sub-module

---
+++ Facebook Mac Core/FaceBookEventManager/EventKitExtension/EventKitExtension/EventKitExtension.h
@@ -0,0 +1,20 @@
+//
+//  EventKitExtension.h
+//  EventKitExtension
+//
+//  Created by Satendra Singh on 27/01/19.
+//  Copyright © 2019 Corebits Software solutions Pvt Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+//! Project version number for EventKitExtension.
+FOUNDATION_EXPORT double EventKitExtensionVersionNumber;
+
+//! Project version string for EventKitExtension.
+FOUNDATION_EXPORT const unsigned char EventKitExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventKitExtension/PublicHeader.h>
+
+
