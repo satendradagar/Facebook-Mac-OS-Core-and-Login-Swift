de2996b22c1170ad340720044418af40512ca082
Satendra Touch <satendradagar@gmail.com>
2019-01-27 15:38:18 +0530

    Implemented Facebook event manager separately

---
+++ Facebook Mac Core/FaceBookEventManager/FacebookEventManager/FacebookEventManager/FacebookEventManager.h
@@ -0,0 +1,19 @@
+//
+//  FacebookEventManager.h
+//  FacebookEventManager
+//
+//  Created by Satendra Singh on 27/01/19.
+//  Copyright © 2019 Corebits Software solutions Pvt Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FacebookEventManager.
+FOUNDATION_EXPORT double FacebookEventManagerVersionNumber;
+
+//! Project version string for FacebookEventManager.
+FOUNDATION_EXPORT const unsigned char FacebookEventManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FacebookEventManager/PublicHeader.h>
+
+
