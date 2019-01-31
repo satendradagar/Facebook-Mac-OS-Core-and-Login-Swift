d7d7d6c781dcc684e2f4923b9013a1048beb94b2
Satendra Touch <satendradagar@gmail.com>
2019-01-31 20:38:35 +0530

    added classes for cocoa extension framework

---
+++ Facebook Mac Core/FaceBookEventManager/CococaExtensions/Cocoa Extensions/Cocoa_Extensions.h
@@ -0,0 +1,19 @@
+//
+//  Cocoa_Extensions.h
+//  Cocoa Extensions
+//
+//  Created by Satendra Singh on 31/01/19.
+//  Copyright © 2019 Corebits Software solutions Pvt Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cocoa_Extensions.
+FOUNDATION_EXPORT double Cocoa_ExtensionsVersionNumber;
+
+//! Project version string for Cocoa_Extensions.
+FOUNDATION_EXPORT const unsigned char Cocoa_ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cocoa_Extensions/PublicHeader.h>
+
+
